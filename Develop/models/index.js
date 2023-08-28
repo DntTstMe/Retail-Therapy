b00e253d846151d516ee29ca93e1525ac6fca602
Alex Tester <lex.tester93@gmail.com>
2023-08-28 13:38:50 -0400

    Added to provided starter code models/index.js, created associations between Products, category, etc.

---
+++ Develop/models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('../../../13/fantastic-umbrella/Develop/models/Product');
+const Category = require('./Category');
+const Tag = require('../../../13/fantastic-umbrella/Develop/models/Tag');
+const ProductTag = require('../../../13/fantastic-umbrella/Develop/models/ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+
+// Categories have many Products
+Category.hasMany(Product);
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through:{
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
